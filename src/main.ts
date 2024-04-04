ba48fe03b6582b0b67bd17e8470b7ca6ca91a664
rosaTome <tomerosalinda@gmail.com>
2024-04-04 13:11:19 +0100

    cardContainer fixed with null check

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,18 @@ interface Pokemon {
 
 function createPokemonCard(pokemon: Pokemon) {
 
-    const cardContainer: HTMLDivElement | null = document.querySelector('.card-container');
+    const cardContainer = document.querySelector('.card-container');
+
+    if (!cardContainer) {
+        console.error("Card container not found!");
+        return;
+    }
 
     const card = document.createElement('div');
     card.classList.add('card');
 
     const image = document.createElement('img');
-    image.src = pokemon.image;
+    image.src = pokemon.sprite;
     image.alt = pokemon.name;
     image.classList.add('card__image');
 
@@ -29,20 +34,14 @@ function createPokemonCard(pokemon: Pokemon) {
     heading.classList.add('card__heading');
 
     const text = document.createElement('p');
-    text.textContent = `Type: ${pokemon.types}`;
+    text.textContent = `Type: ${pokemon.types.join(', ')}`;
     text.classList.add('card__text');
 
     content.appendChild(heading);
     content.appendChild(text);
     content.appendChild(image);
     card.appendChild(content);
-
     cardContainer.appendChild(card);
-
-    if (!cardContainer || !card || !image ||!content || !heading || !text ) {
-        console.error("Card container not found!");
-        return;
-    }
 }
 
 function renderPokemonCards() {
